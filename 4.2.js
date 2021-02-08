1d358a38b9d727e99e239639f848a3fee7a1a52a
natalie <natalie.itzhak@gmail.com>
2021-02-08 13:14:20 +0200

    4.2.js

--- 4.2.js
+++ 4.2.js
@@ -1,54 +1,23 @@
-console.log( "Karin " +  calculateGrade([80, 1,6]));
-console.log("Natalie " + calculateGrade([100, 100,100]));
 
-function calculateGrade(inputMarks){
 
-    var marksSum=0; 
-    var avarage=0;
-  
-    //for (i=0; i<inputMarks.length; i++) {
-    //    marksSum = marksSum + inputMarks[i];
-    //}
+let numberScore= Num => {
 
-    //get avarge
-   // for (let item of inputMarks){
-    //       marksSum+=item;
-    //}
-
-   // avarage= marksSum / inputMarks.length;
-
-    avarage= getAvarage(inputMarks);
-    
-
-    //check for grade
-    if (avarage<59)
-    {
+    if (Num < 64) 
         return "F";
-    }
-    if ((avarage==60) && (avarage<=69))
-    {
+    
+    if ((Num == 65) && (Num<= 69)) 
         return "D";
-    }
-    if ((avarage==70) && (avarage<=79))
-    {
+    
+  else if ((Num == 70) && (Num <= 79)) 
         return "C";
-    }
-    if ((avarage==80) && (avarage<=89))
-    {
+    
+   else if ((Num == 80) && (Num <= 89)) 
         return "B";
-    }
+    
     else
         return "A";
 
-
 }
+console.log(numberScore(Num=80))
 
-function getAvarage(arr){
-   var marksSum=0;
-    for (let item of arr){
-        marksSum+=item;
-     }
 
-    var avarage= marksSum / arr.length;
-    return avarage;
-}
