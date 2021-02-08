e4dc45bca949169fc5e9b9186f688fad551c819c
natalie <natalie.itzhak@gmail.com>
2021-02-08 23:26:02 +0200

    new

---
+++ 5.4.js
@@ -0,0 +1,15 @@
+let color='blue';
+switch(color) {
+    case  'yellow':
+    case  'pink' :
+    case  'orange':
+        console.log( 'light color')
+        break;
+    case 'blue':
+    case  'purple':
+    case 'blue':
+        console.log( 'dark color')
+      break;
+    default:
+      console.log('unknown color')
+  }
