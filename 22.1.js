9c7fdd7c639abb634133ebd926faf8c6804e4808
natalie <natalie.itzhak@gmail.com>
2021-02-24 00:39:16 +0200

    22.1

--- 22.1.js
+++ 22.1.js
@@ -4,23 +4,19 @@ const age=document.querySelector(".age")
 const email=document.querySelector(".email")
 const form=document.querySelector(".registration")
 
-
-function submitForm() {
-return confirm('Do you really want to submit the form?' );
-
-  }
-if (submitForm=="submit"){
-alert('congratulations you successfully sent this form')
-} 
-else if (btn.click!=="submit"){
-    alert('change the information')
-}
-btn.addEventListener("click",submitForm)
-
-btn.addEventListener("click",()=>{
-    myWindow = window.open("", "myWindow", "width=300px,height=100px");
-    myWindow.document.write('your name is :' + fullName.value + 
-'your age is :' + age.value + 'your email is :' + email.value + 
-' for confirmation click submit')
-})
+btn.addEventListener('click', function(event) {
+    event.preventDefault();
+    const send = confirm(`Your details are: ${fullName.value} ${age.value} ${email.value}`);
+    if (send)
+    {
+        if (age.value > 0)
+        {
+            const APPROVEMSG = confirm(`Congratulations you successfully sent this form`);
+        }
+        else
+        {
+            const ERRORMSG = confirm(`Invalid age`);
+        }
+    }
+});
 
