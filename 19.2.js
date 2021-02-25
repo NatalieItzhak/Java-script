a33705cb7f0d3110587b0d32cc924d869e61b0e0
natalie <natalie.itzhak@gmail.com>
2021-02-25 11:56:15 +0200

    19.2

--- 19.2.js
+++ 19.2.js
@@ -1,3 +1,22 @@
-const input=document.querySelector('#input')
-const password = input.value;
-  
+const form = document.querySelector('[name="verify"]');
+const inputs = form.querySelectorAll('.inputs input');
+
+inputs.forEach((el) => {
+    el.addEventListener("keyup", () => {
+        el.nextElementSibling.focus()
+    })
+})
+function toPasteit(e) {
+    const paste = e.clipboardData.getData('text');
+    inputs.forEach((input, i) => {
+        console.log(input);
+        input.value = paste[i] || '';
+    });
+}
+inputs[0].addEventListener('paste', toPasteit);
+
+
+
+
+
+
